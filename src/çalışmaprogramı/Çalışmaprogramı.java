f41db2ce19c85be3ef6893522a48a87ed9adbcef
Eyüp Duruk <147594977+eyupduruk@users.noreply.github.com>
2023-11-12 20:45:40 +0300

    Add files via upload

---
+++ src/çalışmaprogramı/Çalışmaprogramı.java
@@ -0,0 +1,111 @@
+
+package çalışmaprogramı;
+
+import java.util.Scanner;
+
+
+public class Çalışmaprogramı {
+
+   
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);  // scanner değerini import ediniz.
+        System.out.println("--Ders Programına bakmak için gün giriniz.--");
+        String girilengün = input.next();
+     
+        
+        
+        
+        
+        
+  String gun = "Pazartesi";
+  switch (gun) {
+      case "Pazartesi":
+          System.out.println("-Bugün yapılacaklar-");
+          System.out.println("----------------");
+          System.out.println("Çalışmam 1");
+          System.out.println("Çalışmam 2");
+          System.out.println("Çalışmam 3");
+          System.out.println("Çalışmam 4");
+       break;
+      case "Salı":
+          System.out.println("-bugün yapılacaklar-");
+          System.out.println("-----------------");
+          System.out.println("Çalışmam 1");
+          System.out.println("Çalışmam 2");
+          System.out.println("Çalışmam 3");
+          System.out.println("Çalışmam 4");
+          break;
+      case "Çarşamba":
+          System.out.println("-Bugün yapılacaklar-");
+          System.out.println("-----------------");
+         System.out.println("Çalışmam 1");
+          System.out.println("Çalışmam 2");
+          System.out.println("Çalışmam 3");
+          System.out.println("Çalışmam 4");
+          break;
+      case "Perşembe":
+          System.out.println("-Bugün yapılacaklar-");
+          System.out.println("------------------");
+          System.out.println("Çalışmam 1");
+          System.out.println("Çalışmam 2");
+          System.out.println("Çalışmam 3");
+          System.out.println("Çalışmam 4");
+          break;
+      case "Cuma":
+          System.out.println("-Bugün yapılacaklar-");
+          System.out.println("-----------------");
+         System.out.println("Çalışmam 1");
+          System.out.println("Çalışmam 2");
+          System.out.println("Çalışmam 3");
+          System.out.println("Çalışmam 4");
+          break;
+          
+      default:
+          System.out.println("-Bugün yapılacaklar-");
+          System.out.println("-----------------");
+          System.out.println("Çalışmam 1");
+          System.out.println("Çalışmam 2");
+          System.out.println("Çalışmam 3");
+          System.out.println("Çalışmam 4");
+          
+          
+      
+      
+  }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
